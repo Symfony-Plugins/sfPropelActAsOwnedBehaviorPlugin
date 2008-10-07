9bf453a143aa8917200db861380c6155be7e5e7e
florian.klein <florian.klein@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-10-07 06:44:37 -0500

    Created initial structure of a behavior Based on sfParanoidBehaviorPlugin
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelActAsOwnedBehaviorPlugin/trunk@12027 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -1,13 +1,7 @@
 <?php
 
-sfPropelBehavior::registerMethods('paranoid', array(
-  array('sfPropelOwnedBehavior', 'forceDelete'),
+sfPropelBehavior::registerMethods('owned', array(
 ));
 
-sfPropelBehavior::registerHooks('paranoid', array(
-  ':delete:pre'                => array('sfPropelOwnedBehavior', 'preDelete'),
-  'Peer:doSelectRS'            => array('sfPropelOwnedBehavior', 'doSelectRS'),
-  'Peer:doSelectJoin'          => array('sfPropelOwnedBehavior', 'doSelectRS'),
-  'Peer:doSelectJoinAll'       => array('sfPropelOwnedBehavior', 'doSelectRS'),
-  'Peer:doSelectJoinAllExcept' => array('sfPropelOwnedBehavior', 'doSelectRS'),
+sfPropelBehavior::registerHooks('owned', array(
 ));
